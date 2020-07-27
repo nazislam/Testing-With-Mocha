c39a244ac684c8a09cadccf860f9fb31692acb52
Naz Islam <nazislam@MacBook-Pro.local>
2020-07-27 01:06:58 -0400

    program.js update

--- program.js
+++ program.js
@@ -6,11 +6,6 @@ function findGreater(num1, num2) {
     }
 }
 
-function findGreaterSmarter(num1, num2) {
-    return (num1 > num2) ? num1 : num2;
-}
-
 module.exports = { 
-    findGreater,
-    findGreaterSmarter
+    findGreater
 };
--- test.js
+++ test.js
@@ -5,13 +5,7 @@ describe('When calling findGreater', () => {
     it('should return 10 when input: 5, 10', () => {
         assert.equal(10, program.findGreater(5, 10));
     });
-});
-
-describe('When calling findGreaterSmarter', () => {
-    it('should return 20 when input: 10, 20', () => {
-        assert.equal(20, program.findGreaterSmarter(10, 20));
-    });
-    it('should return 20 when input: 10, 20', () => {
-        assert.equal(20, program.findGreaterSmarter(20, 10));
+    it('should return 10 when input: 10, 5', () => {
+        assert.equal(10, program.findGreater(10, 5));
     });
 });
